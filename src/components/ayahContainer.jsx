dea72deb7bbcfad04e759b66f13a8ad7b8300a59
ibn-h <ibnnh.deen@gmail.com>
2025-11-27 19:50:39 +0100

    Ayahs to show prop toegevoegd aan ayahs container

--- src/components/ayahContainer.jsx
+++ src/components/ayahContainer.jsx
@@ -2,13 +2,17 @@ import { useState, useEffect } from "react";
 
 import Ayah from "../components/ayah.jsx";
 
-function AyahContainer({ id, ayahData = {} }) {
+function AyahContainer({
+  id,
+  ayahData = {},
+  ayahsToShow = ayahData.totalAyah,
+}) {
   const inEnglish = ayahData.english || [];
   const inArabic = ayahData.arabic1 || [];
 
   return (
     <div id="ayahs-container" className="flex flex-col gap-4">
-      {inEnglish.map((ayah, index) => (
+      {inEnglish.slice(0, ayahsToShow).map((ayah, index) => (
         <Ayah
           key={index}
           ayahInArabic={inArabic[index]}
--- src/pages/memorizePage.jsx
+++ src/pages/memorizePage.jsx
@@ -1,8 +1,38 @@
-import React from "react";
-import ayahContainer from "../components/ayahContainer.jsx";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import AyahContainer from "../components/ayahContainer.jsx";
+import fetchAyahData from "../utils/fetchAyahData.jsx";
 
 function MemorizePage() {
-  return <div>MemorizePage</div>;
+  const [ayahData, setAyahData] = useState({});
+
+  let { id } = useParams();
+  id = Number(id);
+
+  if (isNaN(id) || id > 114 || id < 1) {
+    return <div>Unable to get Surah</div>;
+  }
+
+  useEffect(() => {
+    fetchAyahData(id)
+      .then((response) => response.json())
+      .then((data) => {
+        setAyahData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, [id]);
+
+  return (
+    <div className="bg-amber-50 min-h-screen p-8">
+      <div className="flex flex-col gap-10 items-center justify-center">
+        <h1 className="text-5xl">Memorize {ayahData.surahNameTranslation}</h1>
+
+        <AyahContainer id={id} ayahData={ayahData} ayahsToShow={1} />
+      </div>
+    </div>
+  );
 }
 
 export default MemorizePage;
